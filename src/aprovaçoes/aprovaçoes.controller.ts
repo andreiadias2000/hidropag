e26fa5afc073a8c1da078c795e6a1dcb8eb81762
Andreia <andreiadias2000.as@gmail.com>
2026-05-03 14:35:08 -0300

    atualizando as entidades e regras de seguranca das aprovacoes

--- src/aprovaçoes/aprovaçoes.controller.ts
+++ src/aprovaçoes/aprovaçoes.controller.ts
@@ -1,34 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// src/aprovaçoes/aprovaçoes.controller.ts
+
+import { Controller, Get, Post, Body, Put, Delete, Param } from '@nestjs/common';
 import { AprovaçoesService } from './aprovaçoes.service';
-import { CreateAprovaçoeDto } from './dto/create-aprovaçoe.dto';
-import { UpdateAprovaçoeDto } from './dto/update-aprovaçoe.dto';
 
 @Controller('aprovaçoes')
 export class AprovaçoesController {
+  
   constructor(private readonly aprovaçoesService: AprovaçoesService) {}
 
   @Post()
-  create(@Body() createAprovaçoeDto: CreateAprovaçoeDto) {
-    return this.aprovaçoesService.create(createAprovaçoeDto);
+  async criar(@Body() dados: any) { 
+    return await this.aprovaçoesService.create(dados);
   }
 
   @Get()
-  findAll() {
-    return this.aprovaçoesService.findAll();
+  async buscarTodos() {
+    return await this.aprovaçoesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.aprovaçoesService.findOne(+id);
+  async buscarUm(@Param('id') id: number) {
+    return await this.aprovaçoesService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAprovaçoeDto: UpdateAprovaçoeDto) {
-    return this.aprovaçoesService.update(+id, updateAprovaçoeDto);
+  @Put(':id')
+  async atualizar(@Param('id') id: number, @Body() dados: any) {
+    await this.aprovaçoesService.update(id, dados);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.aprovaçoesService.remove(+id);
+  async remover(@Param('id') id: number) {
+    await this.aprovaçoesService.remove(id);
   }
 }
+
+//aprovaçoes.controller.ts
+
+// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// import { AprovaçoesService } from './aprovaçoes.service';
+// import { CreateAprovaçoeDto } from './dto/create-aprovaçoe.dto';
+// import { UpdateAprovaçoeDto } from './dto/update-aprovaçoe.dto';
+
+// @Controller('aprovaçoes')
+// export class AprovaçoesController {
+//   constructor(private readonly aprovaçoesService: AprovaçoesService) {}
+
+//   @Post()
+//   create(@Body() createAprovaçoeDto: CreateAprovaçoeDto) {
+//     return this.aprovaçoesService.create(createAprovaçoeDto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.aprovaçoesService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.aprovaçoesService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateAprovaçoeDto: UpdateAprovaçoeDto) {
+//     return this.aprovaçoesService.update(+id, updateAprovaçoeDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.aprovaçoesService.remove(+id);
+//   }
+// }
+
+
--- src/aprovaçoes/entities/aprovaçoe.entity.ts
+++ src/aprovaçoes/entities/aprovaçoe.entity.ts
@@ -1,22 +1,23 @@
-import { timeStamp } from "node:console";
+
+// src/aprovaçoes/entities/aprovaçoe.entity.ts
+
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Notas } from "../../notas-fiscais/entities/notas-fiscais.entity";
 import { Usuarios } from "../../usuarios/entities/usuario.entity";
 
 @Entity('APROVACOES')
+export class APROVACOES {
 
-export class APROVACOES{
-
-    @PrimaryGeneratedColumn('uuid')
+    @PrimaryGeneratedColumn()
     id?: number;
 
-    @Column()
+    // A decisão agora é número: Ex: 1 = Aprovado, 2 = Reprovado
+    @Column({ nullable: true })
     decisao?: number;
 
-    @Column()
+    @Column({ nullable: true })
     observacao?: string;
 
-    
     // Relacionamento: Muitas aprovações podem referenciar a mesma Nota
     @ManyToOne(() => Notas, (nota) => nota.aprovacoes)
     nota?: Notas;
@@ -26,8 +27,44 @@ export class APROVACOES{
     usuario?: Usuarios;
 
     @CreateDateColumn({ type: 'timestamp' })
-    decidido_em?: Date; //[cite: 1]
-
-
-
+    decidido_em?: Date;
 }
+
+
+
+// //aprovaçoes.entity.ts
+
+// import { timeStamp } from "node:console";
+// import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+// import { Notas } from "../../notas-fiscais/entities/notas-fiscais.entity";
+// import { Usuarios } from "../../usuarios/entities/usuario.entity";
+
+// @Entity('APROVACOES')
+
+// export class APROVACOES{
+
+//     @PrimaryGeneratedColumn('uuid')
+//     id?: number;
+
+//     @Column()
+//     decisao?: number;
+
+//     @Column()
+//     observacao?: string;
+
+    
+//     // Relacionamento: Muitas aprovações podem referenciar a mesma Nota
+//     @ManyToOne(() => Notas, (nota) => nota.aprovacoes)
+//     nota?: Notas;
+
+//     // Relacionamento: Muitas aprovações podem ser feitas pelo mesmo Usuário
+//     @ManyToOne(() => Usuarios, (usuario) => usuario.aprovacoes)
+//     usuario?: Usuarios;
+
+//     @CreateDateColumn({ type: 'timestamp' })
+//     decidido_em?: Date; //[cite: 1]
+
+
+
+// }
+
