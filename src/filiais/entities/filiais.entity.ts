866d1635744b3c98e8ec9a9c44e43831d2686233
Andreia <andreiadias2000.as@gmail.com>
2026-05-02 21:51:29 -0300

    Forçando a versão local a ser a oficial do projeto

--- src/filiais/entities/filiais.entity.ts
+++ src/filiais/entities/filiais.entity.ts
@@ -1,48 +1,11 @@
-// //filiais.entity.ts
-// import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-// import { Obras } from "../../obras-empreendimentos/entities/obras-empreendimento.entity";
-// import { Usuarios } from "../../usuarios/entities/usuario.entity";
-
-// @Entity('FILIAIS')
-// export class Filiais{
-//     @PrimaryGeneratedColumn('uuid')
-//     id?: number; 
-
-//     @Column()
-//     nome?: string
-
-//     @Column()
-//     cidade?: string
-
-//     // Relacionamento: Uma filial pode ter muitas obras
-//     @OneToMany(() => Obras, (obra) => obra.filiais)
-//     obras?: Obras[];
-
-//     // Relacionamento: Uma filial pode ter muitos usuários
-//     @OneToMany(() => Usuarios, (usuario) => usuario.filial)
-//     usuarios?: Usuarios[];
-
-// }
-
-
-// filiais.entity.ts
-import { 
-  Column, 
-  Entity, 
-  OneToMany, 
-  ManyToMany, // <-- 1. Adicionamos o ManyToMany aqui em cima
-  PrimaryGeneratedColumn 
-} from "typeorm";
+//filiais.entity.ts
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Obras } from "../../obras-empreendimentos/entities/obras-empreendimento.entity";
 import { Usuarios } from "../../usuarios/entities/usuario.entity";
 
 @Entity('FILIAIS')
 export class Filiais{
-    
-    // o id é 'number' (1, 2, 3), 
-    // tirei a palavra 'uuid' de dentro dos parênteses para não dar erro no banco, 
-    // pois uuid gera textos muito granmdes com letras e números!
-    @PrimaryGeneratedColumn() 
+    @PrimaryGeneratedColumn('uuid')
     id?: number; 
 
     @Column()
@@ -51,13 +14,12 @@ export class Filiais{
     @Column()
     cidade?: string
 
-    // Relacionamento: Uma filial pode ter muitas obras 
+    // Relacionamento: Uma filial pode ter muitas obras
     @OneToMany(() => Obras, (obra) => obra.filiais)
     obras?: Obras[];
 
-    // Relacionamento: Uma filial pode ser acessada por muitos usuários 
-    // Trocamos OneToMany por ManyToMany e avisamos que o outro lado se chama "filiaisPermitidas"
-    @ManyToMany(() => Usuarios, (usuario) => usuario.filiaisPermitidas)
+    // Relacionamento: Uma filial pode ter muitos usuários
+    @OneToMany(() => Usuarios, (usuario) => usuario.filial)
     usuarios?: Usuarios[];
 
 }
--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,90 +1,32 @@
-// // usuario.entity.ts
-// import { Column, Entity, PrimaryGeneratedColumn,OneToMany, ManyToOne, } from "typeorm";
-// import { Filiais } from "../../filiais/entities/filiais.entity";
-// import { APROVACOES } from "../../aprovaçoes/entities/aprovaçoe.entity";
-
-// @Entity()
-// export class Usuarios{
-//     @PrimaryGeneratedColumn()
-//     id?: number;
-
-//     @Column()
-//     nome?: string 
-
-//     @Column()
-//     email?: string;
-
-//     @Column()
-//     senha?: number
-    
-//     @Column()
-//     perfil?: string
-
-//     // Relacionamento: Muitos usuários trabalham em uma Filial
-//     @ManyToOne(() => Filiais, (filial) => filial.usuarios)
-//     filial?: Filiais;
-
-//     // Relacionamento: Um usuário pode realizar várias aprovações
-//     @OneToMany(() => APROVACOES, (aprovacao) => aprovacao.usuario)
-//     aprovacoes?: APROVACOES[];
-
-
-// }
-
 // usuario.entity.ts
-import { 
-  Column, 
-  Entity, 
-  PrimaryGeneratedColumn, 
-  OneToMany, 
-  ManyToMany, 
-  JoinTable 
-} from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn,OneToMany, ManyToOne, } from "typeorm";
 import { Filiais } from "../../filiais/entities/filiais.entity";
 import { APROVACOES } from "../../aprovaçoes/entities/aprovaçoe.entity";
 
-// 1. A LISTA DOS CARGOS/PERFIL
-export enum PerfilUsuario {
-  ADMIN_MASTER = 'admin_master', // Pode tudo e vê todas as filiais
-  DIRETORIA = 'diretoria',       // Vê filiais permitidas e autoriza pagamentos
-  FINANCEIRO = 'financeiro',     // Vê filiais permitidas, lança contas, mas não autoriza
-  OPERADOR = 'operador',         // Lança notas e vê apenas as filiais permitidas
-  VISUALIZADOR = 'visualizador', // Apenas visualiza as filiais permitidas e adiciona comentários
-}
-
-@Entity('usuarios')
-export class Usuarios {
+@Entity()
+export class Usuarios{
     @PrimaryGeneratedColumn()
     id?: number;
 
     @Column()
-    nome?: string;
+    nome?: string 
 
     @Column()
     email?: string;
 
     @Column()
-    senha?: number;
+    senha?: number
     
-    // APLICANDO A TRAVA DO PERFIL
-    @Column({
-        type: 'enum',
-        enum: PerfilUsuario,
-        default: PerfilUsuario.VISUALIZADOR, 
-    })
-    perfil?: PerfilUsuario;
+    @Column()
+    perfil?: string
 
-    // 3. Muitos para Muitos
-    // Relacionamento: Um usuário pode acessar várias Filiais
-    @ManyToMany(() => Filiais)
-    @JoinTable({
-        name: 'usuario_filial_acesso', 
-        joinColumn: { name: 'usuario_id', referencedColumnName: 'id' },
-        inverseJoinColumn: { name: 'filial_id', referencedColumnName: 'id' },
-    })
-    filiaisPermitidas?: Filiais[]; 
+    // Relacionamento: Muitos usuários trabalham em uma Filial
+    @ManyToOne(() => Filiais, (filial) => filial.usuarios)
+    filial?: Filiais;
 
     // Relacionamento: Um usuário pode realizar várias aprovações
     @OneToMany(() => APROVACOES, (aprovacao) => aprovacao.usuario)
     aprovacoes?: APROVACOES[];
+
+
 }
